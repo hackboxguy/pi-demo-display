3ec14bf8c98eeb094d1a3e91a7422223479ccaa4
hackboxguy <albert.david@gmail.com>
2024-10-26 22:06:06 +0200

    included kodi-guisettings.xml

--- setup.sh
+++ setup.sh
@@ -36,10 +36,13 @@ test 0 -eq $? && echo "[OK]" || echo "[FAIL]"
 
 printf "Setting up kodi......................................... "
 runuser -l pi -c 'mkdir -p ~/.kodi/addons/service.autoexec'
+runuser -l pi -c 'mkdir -p ~/.kodi/userdata/addon_data/skin.estuary/'
 runuser -l pi -c 'cp kodi-files/addon.xml ~/.kodi/addons/service.autoexec'
 runuser -l pi -c 'cp kodi-files/autoexec.py ~/.kodi/addons/service.autoexec'
 runuser -l pi -c 'cp kodi-files/favourites.xml ~/.kodi/userdata/'
 runuser -l pi -c 'cp kodi-files/settings.xml ~/.kodi/userdata/addon_data/skin.estuary/'
+runuser -l pi -c 'cp kodi-files/guisettings.xml ~/.kodi/userdata/'
+
 cp kodi-files/*.png /usr/share/kodi/media/
 cp kodi-files/*.jpg /usr/share/kodi/media/
 test 0 -eq $? && echo "[OK]" || echo "[FAIL]"
